a72d2ccae4a81c8bc8cd33ae9b7a5ecf63cc0c19
XECCNDM <XECCNDM@V082SOM111209.ams.bnymellon.net>
2018-10-08 18:16:40 +0530

    Initial commit

---
+++ src/main/java/com/sba/demo/sbapetclinic/SbaPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.sba.demo.sbapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sba/demo/sbapetclinic/SbaPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sba.demo.sbapetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbaPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
